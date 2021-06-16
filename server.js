c41b976a352da4c79ab83188079e5566429d4050
Lam Nhut Tien <Tien>
2021-06-16 20:54:45 +0700

    first commit

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express;
 const path = require("path");
 
-app.use(express.static(_dirname + "/dist"));
-app.listen(process.env.PORT || 8000);
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(_dirname + "/dist/index.html"));
-});
-console.log("Port listen");
+const port = process.env.PORT || 5000;
+var app_path = "../dist/ASM";
+
+var route = require()
+
+app.request(route);
